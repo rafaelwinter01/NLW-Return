b4b4824af17cc136c33077fefce6e36dad2370ea
Rafael Winter <rafael.winter@serpro.gov.br>
2022-05-08 23:25:19 -0300

    V2

--- web/src/components/CloseButton.tsx
+++ web/src/components/CloseButton.tsx
@@ -3,7 +3,7 @@ import { X } from 'phosphor-react'
 
 export function CloseButton() {
     return (
-        <Popover.Button className="top-5 right-5 absolute text-zinc-400 hover:text-zinc-100" text="Fechar Formulário de Feedback">
+        <Popover.Button className="top-5 right-5 absolute text-zinc-400 hover:text-zinc-100" >
             <X weight="bold" className="w-4 h-4" />
         </Popover.Button>
     )
